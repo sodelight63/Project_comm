1c9ee96d37bdb10279a0a699e631b1dd5675dffd
HarisPasival <624234018@parichat.skru.ac.th>
2022-06-03 16:24:50 +0700

    finish1

--- index.php
+++ index.php
@@ -71,7 +71,6 @@ session_start();
             </div>
         </div>
     </div>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-
 </html>
