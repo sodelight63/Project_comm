5cac3281b31b8ce70886036be8cd136736166169
HarisPasival <624234018@parichat.skru.ac.th>
2022-06-08 20:26:48 +0700

    8/6/65

--- index.php
+++ index.php
@@ -17,7 +17,32 @@ session_start();
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.js"></script>
     <script>
         $(document).ready(function() {
-            $('#example').DataTable();
+            $('#example').DataTable({
+                language: {
+                    "decimal": "",
+                    "emptyTable": "ไม่มีข้อมูลในตาราง",
+                    "info": "แสดง _START_ - _END_ จาก _TOTAL_ รายการ",
+                    "infoEmpty": "แสดง 0 - 0 จาก 0 รายการ",
+                    "infoFiltered": "(คัดกรองจาก _MAX_ รายการทั้งหมด)",
+                    "infoPostFix": "",
+                    "thousands": ",",
+                    "lengthMenu": "แสดง _MENU_ รายการ",
+                    "loadingRecords": "Loading...",
+                    "processing": "",
+                    "search": "ค้นหา:",
+                    "zeroRecords": "ไม่พบการค้นหาที่ตรงกัน",
+                    "paginate": {
+                        "first": "First",
+                        "last": "Last",
+                        "next": "หน้าต่อไป",
+                        "previous": "ก่อนหน้า"
+                    },
+                    "aria": {
+                        "sortAscending": ": activate to sort column ascending",
+                        "sortDescending": ": activate to sort column descending"
+                    }
+                }
+            });
         });
     </script>
 </head>
