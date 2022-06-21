4f1d89d4e16955f42bd24be38cb12e545358681b
HarisPasival <624234018@parichat.skru.ac.th>
2022-06-21 14:31:54 +0700

    21/6/65

--- Add_Cus.php
+++ Add_Cus.php
@@ -67,46 +67,6 @@ session_start();
             </div>
         </div>
     </div>
-    <!-- <div class="container">
-        <div class="row">
-            <div class="col-md-8 mt-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h3>เพิ่มข้อมูลลูกค้า
-                        <a href="customer_data.php" class="btn btn-danger float-end">ย้อนกลับ</a>
-                        </h3>
-                    </div>
-                    <div class="card-body">
-                        <form action="crud.php" method="POST">
-                            <div class="col mb-3">
-                                <label>ชื่อ:</label>
-                                <input type="text" name="name_ct" class="form-control" />
-                            </div>
-                            <div class="col mb-3">
-                                <label>นามสกุล:</label>
-                                <input type="text" name="surname_ct" class="form-control" />
-                            </div>
-                            <div class="mb-3">
-                                <label>เบอร์โทร:</label>
-                                <input type="text" name="phone_ct" class="form-control" />
-                            </div>
-                            <div class="mb-3">
-                                <label>อีเมล:</label>
-                                <input type="text" name="email_ct" class="form-control" />
-                            </div>
-                            <div class="mb-3">
-                                <label>ที่อยู่:</label>
-                                <input type="text" name="adress_ct" class="form-control" />
-                            </div>
-                            <div class="mb-3">
-                                <button type="submit" name="save_cus" class="btn btn-primary">เพิ่มข้อมูล</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> -->
     <script src="js/bootstrap.min.js"></script>
 </body>
 
--- Edit_Cus.php
+++ Edit_Cus.php
@@ -80,59 +80,6 @@ include('config/conn.php');
             </div>
         </div>
     </div>
-    <!-- <div class="container">
-        <div class="row">
-            <div class="col-md-8 mt-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h3>แก้ไขข้อมูลลูกค้า
-                            <a href="customer_data.php" class="btn btn-danger float-end">ย้อนกลับ</a>
-                        </h3>
-                    </div>
-                    <div class="card-body">
-                        <?php
-                        if (isset($_GET['customer_id'])) {
-                            $customer_id = $_GET['customer_id'];
-                            $query = "SELECT * FROM customer WHERE customer_id =:customer_id";
-                            $stmt = $conn->prepare($query);
-                            $data = [':customer_id' => $customer_id];
-                            $stmt->execute($data);
-
-                            $result = $stmt->fetch(PDO::FETCH_ASSOC);
-                        }
-                        ?>
-                        <form action="crud.php" method="POST">
-                            <input type="hidden" name="customer_id" value="<?= $result['customer_id'] ?>">
-                            <div class="mb-3">
-                                <label>ชื่อ:</label>
-                                <input type="text" name="name_ct" class="form-control" value="<?= $result['name_ct'] ?>" />
-                            </div>
-                            <div class="mb-3">
-                                <label>นามสกุล:</label>
-                                <input type="text" name="surname_ct" class="form-control" value="<?= $result['surname_ct'] ?>" />
-                            </div>
-                            <div class="mb-3">
-                                <label>เบอร์โทร:</label>
-                                <input type="text" name="phone_ct" class="form-control" value="<?= $result['phone_ct'] ?>" />
-                            </div>
-                            <div class="mb-3">
-                                <label>อีเมล:</label>
-                                <input type="text" name="email_ct" class="form-control" value="<?= $result['email_ct'] ?>" />
-                            </div>
-                            <div class="mb-3">
-                                <label>ที่อยู่:</label>
-                                <input type="text" name="adress_ct" class="form-control" value="<?= $result['adress_ct'] ?>" />
-                            </div>
-                            <div class="mb-3">
-                                <button type="submit" name="edit_cus" class="btn btn-primary">แก้ไขข้อมูล</button>
-                            </div>
-                        </form>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> -->
     <script src="js/bootstrap.min.js"></script>
 </body>
 
